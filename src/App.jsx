613811211a81c5d8b2681d71cee6c6ed366e29d4
GmMamunH <gmmamun3004@gmail.com>
2025-01-12 22:27:04 +0600

    create(redux): create redux folder and relative files

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { Counter } from "./components/Counter";
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-teal-800 text-center py-3">
-        Simple Counter Application using Redux
-      </h1>
-      <Counter />
+      <div>
+        <h1 className="text-3xl text-teal-800 text-center py-3">
+          Simple Counter Application using Redux
+        </h1>
+        <Counter />
+      </div>
     </>
   );
 }
