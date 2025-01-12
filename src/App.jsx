5e14a6ff05ec1952be3f1f99e07bdff0bbfbc713
GmMamunH <gmmamun3004@gmail.com>
2025-01-12 22:49:12 +0600

    wrapper(app): wrapper provider in store

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import { Provider } from "react-redux";
 import { Counter } from "./components/Counter";
+import { store } from "./redux/store";
 
 function App() {
   return (
     <>
-      <div>
-        <h1 className="text-3xl text-teal-800 text-center py-3">
-          Simple Counter Application using Redux
-        </h1>
-        <Counter />
-      </div>
+      <Provider store={store}>
+        <div>
+          <h1 className="text-3xl text-teal-800 text-center py-3">
+            Simple Counter Application using Redux
+          </h1>
+          <Counter />
+        </div>
+      </Provider>
     </>
   );
 }
